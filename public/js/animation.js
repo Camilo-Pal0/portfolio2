7377423d60ce03411dde2eacf71b149a95068634
Camilo Palomeque <unicocamiloenelmundo@gmail.com>
2025-09-12 14:51:48 -0500

    testing 31

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,10 +1,27 @@
 window.sr = ScrollReveal();
 
+//Main
 sr.reveal('header', {
     duration: 3000,
     origin: 'top',
-    distance: '100px'
+    distance: '100px',
+    reset: true 
 });
 sr.reveal('#main-content', {
     duration: 3000,
+    reset: true
+});
+
+//Títulos
+sr.reveal('#experience-title', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px',
+    reset: true
+});
+sr.reveal('#projects-title', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px',
+    reset: true
 });
